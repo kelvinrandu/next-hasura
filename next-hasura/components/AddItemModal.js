cf5ea74ba6fb674f670776f50bfb57693e221cbf
kelvinrandu <randukelvin@gmail.com>
2021-08-04 18:42:25 +0300

    added layout files

---
+++ next-hasura/components/AddItemModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddItemModal = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddItemModal
--- next-hasura/pages/index.js
+++ next-hasura/pages/index.js
@@ -1,16 +1,19 @@
 import Head from "next/head";
 import { useItems } from "../graphql/hooks";
 import { withApollo } from "../graphql/apollo";
-import Image from "next/image";
 import { Text, Flex, Spinner } from "@chakra-ui/react";
-import styles from "../styles/Home.module.css";
+import App from "../components/App";
 
 const Home = () => {
   const { data, loading } = useItems();
 
   const allItems = data ? data.items : [];
   return (
-    <div className={styles.container}>
+    <App width="full" maxWidth="1280px" mx="auto" px={6} py={6}>
+      <Text mb={2} fontSize="sm">
+        {"Active "}
+        <b>{"Items"}</b>
+      </Text>
       {loading ? (
         <Flex pt={24} align="center" justify="center">
           <Spinner size="xl" label="Loading items" />
@@ -24,7 +27,7 @@ const Home = () => {
           )}
         </>
       )}
-    </div>
+    </App>
   );
 };
 export default withApollo(Home, {
